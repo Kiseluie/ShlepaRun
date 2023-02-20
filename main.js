beb7cea32955f72f7d8211a5a903f2267ac04586
Blezz Rot <markus.jenya04@gmail.com>
2023-02-20 10:16:30 +0300

    refactoring

--- main.js
+++ main.js
@@ -32,13 +32,6 @@ function getQuantityElements(heightElement) {
 function startGame(){
     start.classList.add('hide');
     gameArea.innerHTML = '';
-    // for (let i = 0; i < getQuantityElements(100); i++) {
-    //     const line = document.createElement('div');
-    //     line.classList.add('line');
-    //     line.style.top = (i * 100) + 'px';
-    //     line.y = i * 100;
-    //     gameArea.appendChild(line);
-    // }
 
     for (let i = 0; i < getQuantityElements(100 * setting.traffic); i++) {
         const enemy = document.createElement('div');
@@ -46,8 +39,8 @@ function startGame(){
         enemy.y = -100 * setting.traffic * (i + 1);
         enemy.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px';
         enemy.style.top = enemy.y + 'px';
-        enemy.style.background = 'transparent url(./image/image-removebg-preview.png) center / cover no-repeat';
-        gameArea.appendChild(enemy);
+        enemy.style.background = 'transparent url(./imgs/пельмень.png) center / cover no-repeat';
+        gameArea.appendChild(enemy); 
         
     }
     setting.score = 0;
@@ -66,7 +59,7 @@ function playGame(){
     if (setting.start){
         setting.score += setting.speed;
         score.innerHTML = 'SCORE<br>' + setting.score;
-        // moveRoad();
+        
         moveEnemy();
         if (keys.ArrowLeft && setting.x > 0) {
             setting.x -= setting.speed;
@@ -103,17 +96,6 @@ function stopRun(event){
     keys[event.key] = false;
 }
 
-// function moveRoad() {
-//     let lines = document.querySelectorAll('.line');
-//     lines.forEach(function(line) {
-//         line.y += setting.speed;
-//         line.style.top = line.y + 'px';
-
-//         if (line.y >= document.documentElement.clientHeight) {
-//             line.y = -100;
-//         }
-//     });
-// }
 
 function moveEnemy() {
     let enemy = document.querySelectorAll('.enemy');
